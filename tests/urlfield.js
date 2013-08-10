1d7a18532c52c503d5866fb2901fa722bdcf0165
cmisenas <acolyte.camille@gmail.com>
2013-08-10 14:05:57 -0400

    Structuring tests for UrlField object Currently setup does not work yet, still working requirejs

--- tests/urlfield.js
+++ tests/urlfield.js
@@ -1,21 +1,28 @@
 var urlfield = require("../js/urlfield");
 var assert = require("assert");
+var setup = require("./lib/mocha");
 
-describe('test mocha', function(){
-  it('it should run mocha tests when we call mocha', function(){
-    assert.equal(1, 1);
-  })
-})
+var testfield;
 
-describe('User Interface', function(){
-  describe('UrlField', function(){
-    it('should be instantiatable', function(){
-      var urlField = new urlfield.UrlField();
-      assert(urlField);
-    })
-    it('should have a get method', function(){
-      var urlField = new urlfield.UrlField();
-      assert.equal(urlField.get, 'function (){\n  }');
-    })
-  })
-})
+describe('UrlField', function(){
+  setup(function(){
+    testfield = '<input type="text" id="urlfield"/>';
+  });
+
+  it('should be instantiatable', function(){
+    var urlField = new urlfield.UrlField();
+    assert(urlField);
+  });
+
+  it('should have a get method', function(){
+    var urlField = new urlfield.UrlField();
+    assert.equal(typeof urlField.get, 'function');
+  });
+
+  it('should be able to get the value of url input field', function(){
+  });
+
+  it('should be able to return false when url input field value is valid', function(){
+  });
+
+});
