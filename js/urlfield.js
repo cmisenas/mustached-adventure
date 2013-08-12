f8ae8c7acc813759c9ca0b2f523bc537c13c8768
cmisenas <acolyte.camille@gmail.com>
2013-08-12 11:18:59 -0400

    UrlField accepts element now instead of id

--- js/urlfield.js
+++ js/urlfield.js
@@ -1,6 +1,6 @@
 ;(function(exports){
   var UrlField = function(el){
-    this.el = document.getElementById(el);
+    this.el = el;
     this.urlval = this.el.val;
   }
   UrlField.prototype.get = function(){
