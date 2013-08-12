a17926382985784cff509426ff61508dc4b4d4ed
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-08-12 17:58:43 -0400

    base 10 to 64 based shortener and tests

--- js/shortener.js
+++ js/shortener.js
@@ -3,19 +3,31 @@
   var Shortener = function(){}
 
   Shortener.prototype.convert = function(val){
-    var convertedDigits = []
-    while (val < 62){
-      remainder = val % 62;
-      digit = val / 62;
-      val = remainder;
-      convertedDigits.push(intToChar(digit));
+    var base = 62;
+    var convertedDigits = [];
+    if (val === 0){
+        return '0'
     }
-    convertedDigits.push(intToChar(val));
-    return convertedDigits.join('');
+    while (val > 0){
+        var remainder = val % base;
+        convertedDigits.push(this.intToChar(remainder));
+        val = Math.floor(val / base);
+    }
+    return convertedDigits.reverse().join('');
   }
 
-  function intToChar(digit){
-    return String.fromCharCode(97 + digit);
+  Shortener.prototype.intToChar = function(digit){
+    var base = 0;
+    if (digit < 10){
+        return digit;
+    } else if (digit < 36){
+        digit -= 10;
+        base = 65;
+    } else {
+        digit -= 36;
+        base = 97;
+    }
+    return String.fromCharCode(base + digit);
   }
 
   exports.Shortener = Shortener;
---
+++ test/shortener.js
@@ -0,0 +1,31 @@
+var Shortener = require("../js/shortener").Shortener;
+var assert = require("assert");
+var mocha = require("mocha");
+var $ = require("jquery");
+
+describe("Shortener", function(){
+    setup(function(){
+        shortener = new Shortener()
+    });
+    it("should be creatable", function(){
+        assert(shortener);
+    });
+    it("should return appropriate char for number", function(){
+        assert.equal(shortener.intToChar(0), '0');
+        assert.equal(shortener.intToChar(10), 'A');
+        assert.equal(shortener.intToChar(36), 'a');
+    });
+    it("should return the right one digit values", function(){
+        assert.equal(shortener.convert(0), '0');
+        assert.equal(shortener.convert(10), 'A');
+        assert.equal(shortener.convert(36), 'a');
+    });
+    it("should return the right two digit value", function(){
+        assert.equal(shortener.convert(62), '10');
+        assert.equal(shortener.convert(63), '11');
+    });
+    it("should return the right three digit value", function(){
+        assert.equal(shortener.convert(3844), '100');
+        assert.equal(shortener.convert(3845), '101');
+    });
+});
