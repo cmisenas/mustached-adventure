858f791bd03a9aeee5cef6e6f4e1e9d2672168a6
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-08-12 16:33:58 -0400

    basic tests and implementation for url field and checking

--- js/urlfield.js
+++ js/urlfield.js
@@ -1,9 +1,29 @@
 ;(function(exports){
   var UrlField = function(el){
     this.el = el;
-    this.urlval = this.el.val;
   }
   UrlField.prototype.get = function(){
+    var urlVal = this.el.val
+    if (this.isOkayUrl(urlVal)) {
+        return this.el.val;
+    }
+    return false;
   }
+  UrlField.prototype.isOkayUrl = function(urlVal) {
+    var hasWhitespace = /\s/g;
+    var invalidEnding = /\.([a-z]{0,1}||[a-z]{5,})\/?$/;
+    var invalidProtocol = /^(http|https|ftp)\:\/{2}/;
+    if (hasWhitespace.test(urlVal)) {
+        return false;
+    } else if(invalidEnding.test(urlVal)) {
+        return false;
+    } else if(invalidProtocol.test(urlVal)) {
+        return false;
+    } else {
+        return true;
+    }
+    
+  }
+  
   exports.UrlField = UrlField;
 }(this));
--- test/urlfield.js
+++ test/urlfield.js
@@ -10,20 +10,48 @@ describe("UrlField", function(){
     testfield = $("<input type='text' id='urlfield'/>");
   });
 
+  beforeEach(function(){
+    urlField = new urlfield.UrlField(testfield);
+  });
+
   it('should be instantiatable', function(){
-    var urlField = new urlfield.UrlField(testfield);
     assert(urlField);
   });
 
   it('should have a get method', function(){
-    var urlField = new urlfield.UrlField(testfield);
     assert.equal(typeof urlField.get, 'function');
   });
 
   it('should be able to get the value of url input field', function(){
+    testfield.val = "http://www.myurl.com/";
+    assert.equal(urlField.get(), testfield.val);
   });
 
-  it('should be able to return false when url input field value is invalid', function(){
+  describe('good url', function(){
+    it('should accept urls stating with the right protocol', function(){
+        var goodUrlEnding = "://www.badurl.com";
+        var goodProtocols = ["http", "https", "ftp"];
+        for (var i = 0; i < goodProtocols.length; i++) {
+            testfield.val = goodProtocols[i] + goodUrlEnding;
+            assert.equal(urlField.get(), false);
+        }
+    });
   });
 
+  describe('bad url', function(){
+    it('should not accept url with a space', function(){
+        testfield.val = "http://www.bad url.com";
+        assert.equal(urlField.get(), false);
+    });
+    it('should have a 2-4 character domain ending', function(){
+        testfield.val = "http://www.badurl.c";
+        assert.equal(urlField.get(), false);
+        testfield.val = "http://www.badurl.coooooooom";
+        assert.equal(urlField.get(), false);
+    });
+    it('should contain the name of a supported protocol', function(){
+        testfield.val = "httpd://www.badurl.com";
+        assert.equal(urlField.get(), false);
+    });
+  });
 });
