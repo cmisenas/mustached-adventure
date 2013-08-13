83fdc923bc78402b66005cdc82f2e53342132d51
cmisenas <acolyte.camille@gmail.com>
2013-08-13 09:42:07 -0400

    Indentation is now set to 2

--- test/shortener.js
+++ test/shortener.js
@@ -4,34 +4,34 @@ var $ = require("jquery");
 
 describe("Shortener", function(){
 
-    setup(function(){
-        shortener = new Shortener()
-    });
+  setup(function(){
+    shortener = new Shortener()
+  });
 
-    it("should be creatable", function(){
-        assert(shortener);
-    });
+  it("should be creatable", function(){
+    assert(shortener);
+  });
 
-    it("should return appropriate char for number", function(){
-        assert.equal(shortener.intToChar(0), '0');
-        assert.equal(shortener.intToChar(10), 'A');
-        assert.equal(shortener.intToChar(36), 'a');
-    });
+  it("should return appropriate char for number", function(){
+    assert.equal(shortener.intToChar(0), '0');
+    assert.equal(shortener.intToChar(10), 'A');
+    assert.equal(shortener.intToChar(36), 'a');
+  });
 
-    it("should return the right one digit values", function(){
-        assert.equal(shortener.convert(0), '0');
-        assert.equal(shortener.convert(10), 'A');
-        assert.equal(shortener.convert(36), 'a');
-    });
+  it("should return the right one digit values", function(){
+    assert.equal(shortener.convert(0), '0');
+    assert.equal(shortener.convert(10), 'A');
+    assert.equal(shortener.convert(36), 'a');
+  });
 
-    it("should return the right two digit value", function(){
-        assert.equal(shortener.convert(62), '10');
-        assert.equal(shortener.convert(63), '11');
-    });
+  it("should return the right two digit value", function(){
+    assert.equal(shortener.convert(62), '10');
+    assert.equal(shortener.convert(63), '11');
+  });
 
-    it("should return the right three digit value", function(){
-        assert.equal(shortener.convert(3844), '100');
-        assert.equal(shortener.convert(3845), '101');
-    });
+  it("should return the right three digit value", function(){
+    assert.equal(shortener.convert(3844), '100');
+    assert.equal(shortener.convert(3845), '101');
+  });
 
 });
--- test/urlfield.js
+++ test/urlfield.js
@@ -28,14 +28,14 @@ describe("UrlField", function(){
   });
 
   describe('good url', function(){
-    
+
     it('should accept urls stating with the right protocol', function(){
-        var goodUrlEnding = "://www.goodurl.com";
-        var goodProtocols = ["http", "https", "ftp"];
-        for (var i = 0; i < goodProtocols.length; i++) {
-            testfield.val = goodProtocols[i] + goodUrlEnding;
-            assert.equal(urlField.get(), testfield.val);
-        }
+      var goodUrlEnding = "://www.goodurl.com";
+      var goodProtocols = ["http", "https", "ftp"];
+      for (var i = 0; i < goodProtocols.length; i++) {
+        testfield.val = goodProtocols[i] + goodUrlEnding;
+        assert.equal(urlField.get(), testfield.val);
+      }
     });
 
   });
@@ -43,30 +43,30 @@ describe("UrlField", function(){
   describe('bad url', function(){
 
     it('should not accept empty string', function(){
-        testfield.val = "";
-        assert.equal(urlField.get(), false);
+      testfield.val = "";
+      assert.equal(urlField.get(), false);
     });
-    
+
     it('should not accept string with only whitespace', function(){
-        testfield.val = "   ";
-        assert.equal(urlField.get(), false);
+      testfield.val = "   ";
+      assert.equal(urlField.get(), false);
     });
 
     it('should not accept url with a space', function(){
-        testfield.val = "http://www.bad url.com";
-        assert.equal(urlField.get(), false);
+      testfield.val = "http://www.bad url.com";
+      assert.equal(urlField.get(), false);
     });
 
     it('should have a 2-4 character domain ending', function(){
-        testfield.val = "http://www.badurl.c";
-        assert.equal(urlField.get(), false);
-        testfield.val = "http://www.badurl.coooooooom";
-        assert.equal(urlField.get(), false);
+      testfield.val = "http://www.badurl.c";
+      assert.equal(urlField.get(), false);
+      testfield.val = "http://www.badurl.coooooooom";
+      assert.equal(urlField.get(), false);
     });
 
     it('should contain the name of a supported protocol', function(){
-        testfield.val = "httpd://www.badurl.com";
-        assert.equal(urlField.get(), false);
+      testfield.val = "httpd://www.badurl.com";
+      assert.equal(urlField.get(), false);
     });
   });
 
