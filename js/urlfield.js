d03e8f5e0155924c00824a4b39c0068862af9ace
cmisenas <acolyte.camille@gmail.com>
2013-08-13 11:22:13 -0400

    Whitespace

--- js/urlfield.js
+++ js/urlfield.js
@@ -1,7 +1,9 @@
 ;(function(exports){
+
   var UrlField = function(el){
     this.el = el;
   }
+
   UrlField.prototype.get = function(){
     var urlVal = this.el.val
     if (this.isOkayUrl(urlVal)) {
@@ -9,6 +11,7 @@
     }
     return false;
   }
+
   UrlField.prototype.isOkayUrl = function(urlVal) {
     var hasWhitespace = /\s/g;
     var invalidEnding = /\.([a-z]{0,1}||[a-z]{5,})\/?$/;
