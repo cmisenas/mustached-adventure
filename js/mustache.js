9490293bede023c8a2716d6016f0401e9999a40b
cmisenas <acolyte.camille@gmail.com>
2013-08-15 16:16:14 -0400

    Mustache is now getting index get method now resets back to 0 when index is out of bounds

--- js/mustache.js
+++ js/mustache.js
@@ -5,17 +5,25 @@
   };
 
   Mustache.prototype.set = function(url, fn) {
-      var that = this;
-      this.storage.getIndex(function(err, res){
-        var hashed = that.shortener.convert(parseInt(res, 10));
-        that.storage.set(hashed, url, function(err, res){
-          fn(err, hashed);
-        });
+    var that = this;
+    var urlArray = url.split('|');
+    this.storage.getIndex(function(err, res){
+      var hashed = that.shortener.convert(parseInt(res, 10));
+      that.storage.set(hashed, urlArray, function(err, res){
+        fn(err, hashed);
       });
+    });
   }
 
-  Mustache.prototype.get = function(hashedUrl, fn){
-    this.storage.get(hashedUrl, fn)
+  Mustache.prototype.get = function(hashedUrl, index, fn){
+    var that = this;
+    this.storage.sizeOf(hashedUrl, function(err1, size){
+      index = index >= size ? 0 : index;
+      that.storage.get(hashedUrl, index, function(err2, urlValue){
+        fn(err2, urlValue, index + 1);
+      });
+    });
   }
+
   exports.Mustache = Mustache;
 }(this));
--- test/mustache.js
+++ test/mustache.js
@@ -32,12 +32,12 @@ describe("Mustache", function(){
   });
 
   it("should grow a sensible mustache", function(done){
-    var url = "http://google.com/";
+    var url1 = "http://google.com/";
     var url2 = "http://google2.com/";
-    mustache.set(url, function(err, res){
-      assert.equal(res, "0");
-      mustache.set(url2, function(err, res){
-        assert.equal(res, "1");
+    mustache.set(url1, function(err1, res1){
+      assert.equal(res1, "0");
+      mustache.set(url2, function(err2, res2){
+        assert.equal(res2, "1");
         done();
       });
     });
@@ -46,12 +46,25 @@ describe("Mustache", function(){
   it("should get the right address back", function(done){
     var originalUrl = "http://google.com/";
     mustache.set(originalUrl, function(err, hashedUrl){
-      mustache.get(hashedUrl, function(err, returnedUrl){
+      mustache.get(hashedUrl, 0, function(err, returnedUrl, newIndex){
         assert.equal(originalUrl, returnedUrl);
         done();
       });
     });
   });
+
+  it("should go back to first url if the sent index is greater than size", function(done) {
+    var urls = "https://www.google.com/|https://www.hackerschool.com/|https://humbughq.com/|http://gmail.com/";
+    mustache.set(urls, function(err1, hashedUrl){
+      mustache.get(hashedUrl, 4, function(err2, res, newIndex){
+        assert.equal(res, "https://www.google.com/");
+        assert.equal(newIndex, "1");
+        done();
+      });
+    });
+  });
+
+
 });
 
 
