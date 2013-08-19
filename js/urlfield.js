b95c0b89fa211749dcc9d89ee8d97991f87d1e9d
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-08-19 16:40:37 -0400

    re-added checks for http, added non relative path will need to be a feature

--- js/urlfield.js
+++ js/urlfield.js
@@ -15,15 +15,15 @@
   UrlField.prototype.isOkayUrl = function(urlVal) {
     var hasWhitespace = /\s/g;
     var invalidEnding = /\.([a-z]{0,1}||[a-z]{5,})\/?$/;
-    //var invalidProtocol = /^(http|https|ftp)\:\/{2}/;
+    var invalidProtocol = /^(http|https|ftp)\:\/{2}/;
     if (urlVal.length === 0) {
         return false;
     } else if (hasWhitespace.test(urlVal)) {
         return false;
     } else if(invalidEnding.test(urlVal)) {
         return false;
-    //} else if(invalidProtocol.test(urlVal) === false) {
-    //    return false;
+    } else if(invalidProtocol.test(urlVal) === false) {
+        return false;
     } else {
         return true;
     }
